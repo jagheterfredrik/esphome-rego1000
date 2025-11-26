44a30d17c25b07268595dd71efd8996d2d9b3216
Fredrik Gustafsson <jagheterfredrik@gmail.com>
2025-11-26 15:47:20 +0100

    fix for esphome 2025.11 changes (#33)
    * fix for esphome 2025.11 changes
    
    * bump Python version
    
    * use python 3.13 as latest supported by esphome

--- components/rego1000/climate/rego_climate.h
+++ components/rego1000/climate/rego_climate.h
@@ -22,7 +22,7 @@ public:
   ClimateTraits traits() override {
     // The capabilities of the climate device
     auto traits = climate::ClimateTraits();
-    traits.set_supports_current_temperature(true);
+    traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
     traits.set_supported_modes({climate::CLIMATE_MODE_HEAT});
     traits.set_visual_min_temperature(12.0);
     traits.set_visual_max_temperature(25.0);
